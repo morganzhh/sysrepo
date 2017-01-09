abfa9ef844aab2f9de2a75ebe6dadfbe8d4dcdd7
Morgan (Han) Zhang <morganzhang@juniper.net>
2017-01-09 11:28:26 +0800

    temp fix for issue#6, will rollback once sysrepo has a fix

--- src/module_dependencies.c
+++ src/module_dependencies.c
@@ -1696,7 +1696,8 @@ md_traverse_schema_tree(md_ctx_t *md_ctx, md_module_t *module, struct lys_node *
             }
 next_node:
             /* backtracking + automatically moving to the next sibling if there is any */
-            if (node != root) {
+//            if (node != root) {
+            if (strcmp(node->name, root->name)) {
                 if (node->next && main_module_schema == LYS_MAIN_MODULE(node->next)) {
                     node = node->next;
                     process_children = true;
